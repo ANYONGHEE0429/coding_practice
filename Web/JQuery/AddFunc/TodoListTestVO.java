108909e19fff5341c9454f4dd9bbf96b1ff989f9
Yonghee <53254016+yonghee-octocat@users.noreply.github.com>
2019-08-08 22:51:12 +0900

    Update TodoListTestVO.java

--- Web/JQuery/AddFunc/TodoListTestVO.java
+++ Web/JQuery/AddFunc/TodoListTestVO.java
@@ -1,8 +1,8 @@
 package com.todoList.web.vo;
 
-import lombok.AllArgsConstructor;
+import lombok.AllArgsConstructor; //모든 필드 값을 파라미터로 받는 생성자를 생성
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.NoArgsConstructor; //파라미터가 없는 기본 생성자를 생성
 
 @Data
 @AllArgsConstructor
